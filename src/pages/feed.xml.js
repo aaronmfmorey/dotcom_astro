273f95e04178433ab9963ba78f903dbb24253237
Aaron Morey <aaronmorey@gmail.com>
2025-03-23 01:16:40 -0500

    Add RSS feed

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,11 +1,21 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-        return rss({
-                title: 'Aaron Morey Dot Com',
-                description: 'My Website', // TODO AMM move this to config and make it better
-                site: context.site,
-                items: await pagesGlobToRssItems(import.meta.glob('./posts/**/*.md')),
-                customData: `<language>en-us</language>`,
-        });
+    let blog = await getCollection('blog');
+    // TODO AMM - put date sort into a util function
+    blog.sort((a, b) => new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime());
+    blog = blog.slice(0,19);
+    return rss({
+        // TODO AMM put this into a config?
+        title: 'Aaron Morey Dot Com',
+        description: 'Posts on AaronMorey.com', // TODO AMM move this to config and make it better
+        site: context.site,
+        items: blog.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            link: `/blog/${post.id}/`,
+        })),
+        customData: `<language>en-us</language>`,
+    });
 }

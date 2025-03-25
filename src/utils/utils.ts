4fb407790e88b0d4db75c6328e46539e6fc9a87a
Aaron Morey <aaronmorey@gmail.com>
2025-03-25 01:23:58 -0500

    Initial effort at reading list

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,12 @@ export const slugify = (text :string) => {
         return text.toLowerCase().replaceAll(/\s/g, "-");
     }
 };
+
+export const formatDateLong = function(date: Date): String {
+    const dateOptions: object = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+    return new Date(date).toLocaleDateString("en-US", dateOptions);
+}

b482067f1443fc146c2fd78cda3080a5ed35992b
Aaron Morey <aaronmorey@gmail.com>
2025-03-26 23:58:44 -0500

    Initial commit of books

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,3 +14,9 @@ export const formatDateLong = function(date: Date): String {
     };
     return new Date(date).toLocaleDateString("en-US", dateOptions);
 }
+
+export const getAllTagsForAuthorMatch = function(allPosts: Array<any>): Array<string> {
+    let allTags = allPosts.map((post) => post.data.tags).flat();
+    return [... new Set(allTags)].filter(tag => tag !== null)
+        .map((tag :string) => tag.toLowerCase().replaceAll(" ", ""));
+}

25f445d3e1d4cccccf117045fa6f4f3917ac47e7
Aaron Morey <aaronmorey@gmail.com>
2025-04-17 13:00:45 -0500

    Add anchors to headings (#15)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,37 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+// CREDIT: https://caseyjamesperno.com/blog/astro-header-anchors/
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://aaronmorey.com",
   trailingSlash: 'ignore',
+
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          content: {
+            type: 'text',
+            value: '§',
+          },
+          headingProperties: {
+            className: ['anchor'],
+          },
+          properties: {
+            className: ['anchor-link'],
+          },
+        },
+      ],
+    ],
+  },
 
   vite: {
     resolve: {

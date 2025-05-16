21224549e77b0b1df3574b11f7cb3eda94711998
Aaron Morey <aaronmorey@gmail.com>
2025-05-15 23:16:30 -0500

    Use rendered body in feed where available

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -19,12 +19,16 @@ export async function GET(context) {
         title: 'Aaron Morey Dot Com',
         description: 'Posts on AaronMorey.com', // TODO AMM move this to config and make it better
         site: context.site,
-        items: rssContent.map((post) => ({
-            title: post.data.title ?? "New Micropost",
-            pubDate: post.data.pubDate ?? post.data.date,
-            link: post.data.pubDate ? `/posts/${post.id}/` : `/micros/${post.id}`,
-            description: post.data.body ?? post.body ?? "",
-        })),
+        items: rssContent.map(function (post) {
+            console.log(post);
+               return ({
+                    title: post.data.title ?? "New Micropost",
+                    pubDate: post.data.pubDate ?? post.data.date,
+                    link: post.data.pubDate ? `/posts/${post.id}/` : `/micros/${post.id}`,
+                    description: post.rendered?.html ?? post.body ?? "",
+                })
+            }
+        ),
         customData: `<language>en-us</language>`,
     });
 }

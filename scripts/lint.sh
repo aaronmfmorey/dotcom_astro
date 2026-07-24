726cd4e32ec7b54bb16139d6a0fe706a74de7f1a
Aaron Morey <aaronmorey@gmail.com>
2026-07-23 22:56:43 -0500

    Linting cleanup

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,6 +2,6 @@
 git submodule update --remote --recursive
 astro build > /dev/null
 echo "Hyperlink check: "
-hyperlink dist/ --sources src/ --check-anchors
+hyperlink dist/ --sources src/ --check-anchors | grep -v "pagefind" | grep -v "dist/" | grep -v "^$"
 echo "Stylelint notes: "
 npx stylelint "src/**/*.css"

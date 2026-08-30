d743f9d8af8dc1dbe1a6752f1c47c3232aced14f
Aaron Morey <aaron@aaronmorey.org>
2026-08-29 23:23:26 -0500

    Add pages to yearly header sum  - Convert data to use TS types

--- src/types/components.ts
+++ src/types/components.ts
@@ -4,3 +4,20 @@ export interface DividerProps {
     dividerWidth: string,
     colorMode: ColorMode;
 }
+
+export interface GoodreadsBook {
+    book_id: number,
+    title: string,
+    author: string,
+    original_publication_year: number,
+    isbn: string, // Can we put more validation on this?
+    isbn13: string,
+    date_read: string, // Format: '2026-08-29 00:00:00'
+    my_rating: number,
+    my_review: string,
+    number_of_pages: number,
+    id: number,
+    bookshop_slug: string,
+    no_bookshop: boolean,
+    running_total: number
+}
